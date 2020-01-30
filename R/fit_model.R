c5686d2a6a314f089ab28d591207b117b2f85972
georgieevans <georginaevans@g.harvard.edu>
2020-01-30 08:21:52 -0500

    First commit

---
+++ R/fit_model.R
@@ -0,0 +1,223 @@
+#' OLS dififferential privacy
+#'
+#' This function works similarly to \code{\link[base]{lm}}. It takes a formula and data and
+#' returns an lmdp object containing bias corrected OLS coefficients and standard errors. The output can be summarised
+#' by inputting it as an argument to \code{\link{summary.lmdp}}.
+#'
+#' @param formula An \code{\link[base]{lm}} style formula.
+#' @param data The data to estimate the model on. The first row of data should contain the DP
+#' error associated with that column.
+#' @param bootstrap_var If FALSE, then the variance is estimated via simulation.
+#' If TRUE then the variance covariance matrix is estimated via bootstrap methods. Default is
+#' FALSE unless model contains interaction terms/squared terms or fewer than 10000 obsesrvations
+#' @param nsims_var Number of bootstrap samples/simulations. Default is 500
+#' @return
+#' Returns a an object of class lmdp containing:
+#' \item{b}{Inconsistent OLS coefficient estimate}
+#' \item{b_vcov}{Estimate of variance covariance of b}
+#' \item{beta_tilde}{Consistent estimates of coefficients, \eqn{\tilde{\beta}}}
+#' \item{beta_tilde_vcov}{Estimate of variance covariance of \eqn{\tilde{\beta}}}
+#' \item{var_sims}{Full set of simulated/bootstrap estimates of b and \eqn{\tilde{\beta}}}
+#' \item{Sigma_sq_hat}{Estimate of \eqn{\sigma^2}}
+#' \item{vc_pos_def}{Indicator variable = 1 if covariance estimate was PD. NA if bootstrap used}
+#' \item{boot}{Indicator variable = 1 if bootstrap was used to estimate variance}
+#' @export
+
+lmdp <- function(formula, data, bootstrap_var = FALSE, nsims_var = 500)
+  {
+
+  # Construct S vector
+  S_vec <- as.numeric(data[1, ])
+
+  # Remove error row
+  data <- data[-1, ]
+
+  # Run OLS
+  reg <- lm(formula, data = data)
+  b <- coef(reg)
+
+  # Extract X matrix and Y vector
+  X <- as.matrix(model.matrix(reg))
+  Y <- as.numeric(model.frame(reg)[, 1])
+
+  # Save inconsistent OLS variance
+  names(b) <- colnames(X)
+  b_var <- vcov(reg)
+
+  # Testing for interactions/ squared variables
+  int_vars <- grep(':', colnames(X), value=TRUE)
+  sq_vars <-  grep("\\^2", colnames(X), value = TRUE)
+  int1 <- NA
+  int2 <- NA
+  index_sq <- NA
+
+  if(length(int_vars) > 1){
+    stop("Model cannot contain more than one interaction variable")
+  }
+
+  if(length(sq_vars) > 1){
+    stop("Model cannot contain more than one squared variable")
+  }
+
+  if(length(int_vars) > 0 & length(sq_vars) > 0){
+    stop("Model cannot contain interaction and squared variable")
+  }
+
+  boot <- TRUE
+  vc_pos_def <- NA
+
+  # No squared or interaction terms
+
+  if(length(int_vars) == 0 & length(sq_vars) == 0){
+
+    # Construct S matrix
+    S <- matrix(0, nrow = ncol(X), ncol = ncol(X))
+
+    # Find position of relevant columns
+    err_ind <- match(colnames(X)[-1], colnames(data))
+    diag(S) <- c(0, S_vec[err_ind])
+
+    # Bias correct
+
+    bias_correct <- betaTilde(coef = b, S = S, X = X, N = N)
+    beta_tilde <- bias_correct$bias_correct
+    x_prime_x <- bias_correct$x_prime_x
+
+    # Estimate sigma^2
+    sigma_sq <- sigmaSq(Y = Y, bias_correct = beta_tilde, S = S, X = X)
+
+    # Estimate variance
+    b_index <- 1:length(b)
+    beta_tilde_index <- (length(b) + 1):(2*length(b))
+
+    if(bootstrap_var == FALSE & N > 9999){
+
+      x_y <- t(X)%*%Y
+
+      var_sims <-  varianceMVN(X = X, Y = Y, sigma_sq = sigma_sq,
+                               nsims = nsims_var, coef = b, S = S, N = N,
+                               x_prime_x = x_prime_x, x_y = x_y)
+
+      # Was vc est PD
+      vc_pos_def <- is.positive.definite(var_sims$Sigma[-1, -1])
+
+      if(vc_pos_def == FALSE){
+        warning("VC matrix not positive definite")
+      }
+
+      # b vcov
+      b_vcov <- cov(var_sims$sims[, b_index])
+
+      # Beta tilde vcov
+      beta_tilde_vcov <- cov(var_sims$sims[, beta_tilde_index])
+      var_sims <- var_sims$sims
+      boot <- FALSE
+
+    }else{
+
+      var_sims <- varianceBoot(Y = Y, X = X, S = S, nsims = nsims_var, N = N, int_vars = int_vars,
+                               sq_vars = sq_vars, int1 = int1, int2 = int2, index_sq = index_sq)
+
+      # b vcov
+      b_vcov <- cov(var_sims[, b_index])
+
+      # Beta tilde vcov
+      beta_tilde_vcov <- cov(var_sims[, beta_tilde_index])
+    }
+
+  }else{
+
+    if(length(int_vars) > 0){
+
+      # Interaction model
+      int_names <- unlist(strsplit(int_vars, split = ":"))
+      int1 <- which(colnames(X) == int_names[1])
+      int2 <- which(colnames(X) == int_names[2])
+
+      # Construct S matrix
+      S <- matrix(0, nrow = ncol(X), ncol = ncol(X))
+
+      # Find position of relevant columns
+      err_ind <- match(colnames(X)[-c(1, ncol(X))], colnames(data))
+      diag(S) <- c(0, S_vec[err_ind], 0)
+
+      # Bias correct
+      bias_correct <- betaTildeInt(coef = b, S = S, X = X, N = N, int1 = int1, int2 = int2)
+      beta_tilde <- bias_correct$bias_correct
+      x_prime_x <- bias_correct$x_prime_x
+
+      # Variance
+      b_index <- 1:length(b)
+      beta_tilde_index <- (length(b) + 1):(2*length(b))
+      var_sims <- varianceBoot(Y = Y, X = X, S = S, nsims = nsims_var, N = N, int_vars = int_vars,
+                               sq_vars = sq_vars, int1 = int1, int2 = int2, index_sq = index_sq)
+      b_vcov <- cov(var_sims[, b_index])
+      beta_tilde_vcov <- cov(var_sims[, beta_tilde_index])
+
+    } else{
+      # Squared model
+      sq_var <- gsub("I\\(", "", sq_vars)
+      sq_var <- gsub("\\^2\\)", "", sq_var)
+      index_sq <- which(colnames(X) == sq_var)
+
+      # Construct S matrix
+      S <- matrix(0, nrow = ncol(X), ncol = ncol(X))
+
+      # Find position of relevant columns
+      err_ind <- match(colnames(X)[-c(1, ncol(X))], colnames(data))
+      diag(S) <- c(0, S_vec[err_ind], 0)
+
+      # Bias correct
+      bias_correct <- betaTildeSq(coef = b, S = S, X = X, N = N, index_sq = index_sq)
+      beta_tilde <- bias_correct$bias_correct
+      x_prime_x <- bias_correct$x_prime_x
+
+      # Variance
+      b_index <- 1:length(b)
+      beta_tilde_index <- (length(b) + 1):(2*length(b))
+      var_sims <- varianceBoot(Y = Y, X = X, S = S, nsims = nsims_var, N = N, int_vars = int_vars,
+                               sq_vars = sq_vars, int1 = int1, int2 = int2, index_sq = index_sq)
+      b_vcov <- cov(var_sims[, b_index])
+      beta_tilde_vcov <- cov(var_sims[, beta_tilde_index])
+
+    }
+  }
+
+  output <- list(
+    b = b,
+    b_vcov = b_vcov,
+    beta_tilde = beta_tilde,
+    beta_tilde_vcov = beta_tilde_vcov,
+    var_sims = var_sims,
+    Sigma_sq_hat = sigma_sq,
+    vc_pos_def = vc_pos_def,
+    boot = boot
+  )
+
+  class(output) <- "lmdp"
+
+  return(invisible(output))
+  }
+
+
+
+#' Summary lmdp
+#'
+#' This function takes an lmdp object as input and produces a summary table comparable to
+#' a summary of an \code{\link[base]{lm}} object.
+#'
+#' @param lmdp_object Output from \code{\link{lmdp}}
+#' @return Table of consistent coefficient estimates and corrresponding standard errors, t-stats
+#' and p-values.
+#' @export
+summary.lmdp <- function(lmdp_object)
+  {
+  beta_tilde <- lmdp_object$beta_tilde
+  beta_tilde_se <- sqrt(diag(lmdp_object$beta_tilde_vcov))
+  t_val <- beta_tilde/beta_tilde_se
+  p_val <- 2*(1 - pnorm(abs(t_val)))
+  mod_output <- cbind(beta_tilde, beta_tilde_se, t_val, p_val)
+  rownames(mod_output) <- names(beta_tilde)
+  colnames(mod_output) <- c('Estimate', 'Std. Error', 't value', 'Pr(>|t|)')
+  return(round(mod_output, 4))
+  }
---
+++ R/utility_functions.R
@@ -0,0 +1,326 @@
+#' Bias Correct OLS
+#'
+#' This function takes OLS coefficients as inputs and bias corrects according to the noise matrix, S.
+#' @param coef Vector of OLS coefficients
+#' @param S Matrix of error terms. Dimensions must be length(coef) x length(coef)
+#' @param X Design matrix that produced OLS coefficients
+#' @param N Number of rows in X
+#' @return
+#' Returns a list:
+#' \item{bias_correct}{Vector of bias corrected coefficients}
+#' \item{x_prime_x}{X'X matrix to be used in variance estimationo}
+#' @export
+betaTilde <- function(coef, S, X, N){
+  Q <- t(X)%*%X
+  omega <- (1/N)*Q - S
+  C <- solve(Q/N)%*%omega
+  bias_correct  <- as.numeric(solve(C)%*%coef)
+  names(bias_correct) <- names(coef)
+  return(list(bias_correct = bias_correct, x_prime_x = Q))
+}
+
+
+#' Bias Correct OLS model with interaction term
+#'
+#' This function takes OLS coefficients from a moel including an
+#' interaction term as inputs and bias corrects according to the noise matrix, S.
+#' @param coef Vector of OLS coefficients
+#' @param S Matrix of error terms. Dimensions must be length(coef) x length(coef)
+#' @param X Design matrix that produced OLS coefficients
+#' @param N Number of rows in X
+#' @param int1 Column index of first interaction variable in X
+#' @param int2 Column index of second interaction variable in X
+#' @return
+#' Returns a list:
+#' \item{bias_correct}{Vector of bias corrected coefficients}
+#' \item{x_prime_x}{X'X matrix to be used in variance estimationo}
+#' @export
+betaTildeInt <- function(coef, S, X, N, int1, int2){
+  # Construct Omega
+  Q <- t(X)%*%X
+  omega <- (1/N)*Q - S
+  int_col <- ncol(X)
+  omega[int1, int_col] <- omega[int1, int_col] - S[int1, int1]*mean(X[, int2])
+  omega[int_col, int1] <- omega[int1, int_col]
+  omega[int2, int_col] <- omega[int2, int_col] - S[int2, int2]*mean(X[, int1])
+  omega[int_col, int2] <- omega[int2, int_col]
+  mu1 <- mean(X[, int1]^2) - S[int1, int1]
+  mu2 <- mean(X[, int2]^2) - S[int2, int2]
+  omega[int_col, int_col] <- omega[int_col, int_col] - S[int2, int2]*S[int1, int1] - S[int2, int2]*mean(mu1) - S[int1, int1]*mean(mu2)
+
+  # Bias correct
+  C <- solve(Q/N)%*%omega
+  bias_correct  <- as.numeric(solve(C)%*%coef)
+  names(bias_correct) <- names(coef)
+  return(list(bias_correct = bias_correct, x_prime_x = Q))
+}
+
+
+#' Bias Correct OLS model with squared variable
+#'
+#' This function takes OLS coefficients from a model including
+#' a squared variable as inputs and bias corrects according to the noise matrix, S
+#' @param coef Vector of OLS coefficients
+#' @param S Matrix of error terms. Dimensions must be length(coef) x length(coef)
+#' @param X Design matrix that produced OLS coefficients
+#' @param N Number of rows in X
+#' @param index_sq Column index of X covariate that is squared
+#' @return
+#' Returns a list:
+#' \item{bias_correct}{Vector of bias corrected coefficients}
+#' \item{x_prime_x}{X'X matrix to be used in variance estimation}
+#' @export
+betaTildeSq <- function(coef, S, X, N, index_sq){
+
+  # Construct X'Z
+  Q <- t(X)%*%X
+  X_Z <- Q - N*S
+  K <- ncol(X)
+  sq_col <- ncol(X)
+  X_Z[sq_col, index_sq] <- X_Z[sq_col, index_sq] - 2*N*S[index_sq, index_sq]*mean(X[, index_sq])
+  non_sqcol <- c(1:(index_sq-1), (index_sq + 1):(K-1))
+  non_sqcol <- non_sqcol[-which(non_sqcol == sq_col|non_sqcol == index_sq)]
+  for(i in non_sqcol){
+    X_Z[i, sq_col] <- X_Z[i, sq_col] - N*S[index_sq, index_sq]*mean(X[, i])
+  }
+  X_Z[index_sq, sq_col] <- X_Z[index_sq, sq_col] - 3*N*S[index_sq, index_sq]*mean(X[, index_sq])
+  mu2 <- mean(X[, index_sq]^2) - S[index_sq, index_sq]
+  X_Z[sq_col, sq_col] <- X_Z[sq_col, sq_col] - 5*N*S[index_sq, index_sq]*mu2 - 3*N*(S[index_sq, index_sq])^2
+  omega <- X_Z/N
+
+  # Bias correct
+  C <- solve(Q/N)%*%omega
+  bias_correct  <- as.numeric(solve(C)%*%coef)
+  names(bias_correct) <- names(coef)
+  return(list(bias_correct = bias_correct, x_prime_x = Q))
+}
+
+
+#' Estimates \eqn{\sigma^2} from regression
+#'
+#' This function takes OLS coefficients and a Y vector to estimate \eqn{\sigma^2}
+#' for the model.
+#' @param Y Dependent variable from OLS model
+#' @param bias_corrrect Bias corrected OLS coefficients
+#' @param S Matrix of error terms. Dimensions must be length(coef) x length(coef)
+#' @param X Design matrix that produced OLS coefficients
+#' @return Estimate of \eqn{\sigma^2}
+#' @export
+sigmaSq <- function(Y, bias_correct, S, X){
+  var(Y - as.numeric(bias_correct%*%t(X))) - t(bias_correct^2)%*%diag(S)
+}
+
+#' Estimates \eqn{Cov(X_k'y, X_j'X_m)}
+#'
+#' This function estimates components of variance-covariance matrix to subsequently
+#' draw random variable of \eqn{\tilde{\beta}} from a multivariate normal.
+#' @param k column index of first X.
+#' @param j column index of second X.
+#' @param m column index of third X.
+#' @param S Matrix of error terms. Dimensions must be ncol(X) x ncol(X)
+#' @param X Design matrix that produced OLS coefficients
+#' @param Y Dependent variable from OLS model
+#' @param N Number of rows in X
+#' @return Estimate of Cov(X_k'y, X_j'X_m)
+#' @export
+covXyXX <- function(k, j, m, S, X, Y, N){
+  cov_est <-  Y%*%X[, m]%*%S[k,j] + Y%*%X[, j]%*%S[k,m]
+  return(cov_est)
+}
+
+
+#' Estimates \eqn{Cov(X_k'y, X_j'y)}
+#'
+#' This function estimates components of variance-covariance matrix to subsequently
+#' draw random variable of \eqn{\tilde{\beta}} from a multivariate normal.
+#' @param k column index of first X.
+#' @param j column index of second X.
+#' @param S Matrix of error terms. Dimensions must be ncol(X) x ncol(X)
+#' @param X Design matrix that produced OLS coefficients
+#' @param Y Dependent variable from OLS model
+#' @param sigma_sq estimate of \Sigma^2
+#' @param x_prime_x X'X
+#' @param N Number of rows in X
+#' @return Estimate of Cov(X_k'y, X_j'y)
+#' @export
+covXyXy <- function(k, j, S, X, Y, sigma_sq, x_prime_x, N){
+  Z_prime_Z <- x_prime_x - N*S
+  cov_est <- sigma_sq*Z_prime_Z[k,j] +  S[k, j]*(t(Y)%*%Y)
+  return(cov_est)
+}
+
+#' Estimates \eqn{Cov(X_k'X_j, X_m'X_l)}
+#'
+#' This function estimates components of variance-covariance matrix to subsequently
+#' draw random variable of \eqn{\tilde{\beta}} from a multivariate normal.
+#' @param k column index of first X.
+#' @param j column index of second X.
+#' @param m column index of third X.
+#' @param l column index of fourth X.
+#' @param S Matrix of error terms. Dimensions must be ncol(X) x ncol(X)
+#' @param X Design matrix that produced OLS coefficients
+#' @param x_prime_x X'X
+#' @param N Number of rows in X
+#' @return Estimate of Cov(X_k'X_j, X_m'X_{\ell})
+#' @export
+covXX <- function(k, j, l, m, S, X, x_prime_x, N){
+  omega <- x_prime_x - N*S
+  cov_est <- omega[k,l]*S[j, m] + omega[k, m]*S[j,l] + omega[j, l]*S[k,m] + omega[j, m]*S[k,l]  + 2*N*(S[k,l])*(S[j,m])
+  return(cov_est)
+}
+
+#' Draw re-sampled estimates
+#'
+#' This function draws random variables vec(X'X, X'y) from a multivariate normal
+#' and constructs a simulated \eqn{\tilde{\beta}} and b.
+#' @param X Design matrix that produced OLS coefficients
+#' @param mu Mean vector for multivariate normal
+#' @param Sigma Variance covariance matrix for mjultivariate normal
+#' @param S Matrix of error terms. Dimensions must be ncol(X) x ncol(X)
+#' @param N Number of rows in X
+#' @return One draw of vector of re-sample b and \eqn{\tilde{\beta}}
+#' @export
+simulateEst <- function(X, mu, Sigma, S, N){
+
+  Sigma <- as.matrix(Matrix::nearPD(Sigma[-1, -1])$mat)
+  draws <- c(mu[1], mvtnorm::rmvnorm(1, mean = mu[-1], sigma = Sigma)) # removing the column of 1's
+  K <- ncol(X)
+
+  # Construct X'X
+  Q_star <- matrix(0, K, K)
+  to <- length(draws) - ncol(X)
+  draws_x_x <- draws[c(1:to)]
+  upperTriangle(Q_star, diag = TRUE, byrow = TRUE) <- draws_x_x
+  Q_star <- as.matrix(Matrix::forceSymmetric(Q_star, uplo="U"))
+
+  # Construct X'y
+  from <- length(draws) - ncol(X) + 1
+  to <- length(draws)
+  X_y <- draws[c(from:to)]
+
+  # b star
+  b_star <- as.numeric(solve(Q_star)%*%as.matrix(X_y))
+
+  # beta tilde star
+  omega_star <- (1/N)*Q_star - S
+  C_star <- solve(Q_star/N)%*%omega_star
+  bias_correct  <- as.numeric(solve(C_star)%*%b_star)
+
+  # Bias correct
+  bc_star <- as.numeric(solve(C_star)%*%b_star)
+  bc_star <- c(b_star, bc_star)
+
+  return(bc_star)
+}
+
+#' Simulate variance
+#'
+#' This function takes repeated draws from a MVN to simulate the approximate
+#' distribution of \eqn{\tilde{\beta}} and b.
+#' @param X Design matrix that produced OLS coefficients
+#' @param Y Dependent variable from OLS model
+#' @param sigma_sq Estimate of \eqn{\sigma^2}
+#' @param nsims Number of times to draw from a MVN
+#' @param coef Vector of OLS coefficientss
+#' @param S Matrix of error terms. Dimensions must be length(coef) x length(coef)
+#' @param N Number of rows in X
+#' @param x_prime_x X'X
+#' @param x_y X'y
+#' @return
+#' Returns a list:
+#' \item{sims}{Draws of b and \eqn{\tilde{\beta}}}
+#' \item{Sigma}{Estimate of variance-covariance that was used for simulation}
+#' @export
+varianceMVN <- function(X, Y, sigma_sq, nsims, coef, S, N, x_prime_x, x_y){
+
+  # Extract upper triangle
+  x_x <- unlist(lapply(1:nrow(x_prime_x), FUN = function(i) as.numeric(x_prime_x[i, c(i:ncol(x_prime_x))])))
+
+  # Mean vector
+  mu <- c(x_x, x_y)
+
+  # Initialise matrix
+  dim <- ncol(X)
+  size <- (1/2)*dim*(dim+1) + dim
+  threshold <- (1/2)*dim*(dim+1) + 1
+  Sigma <- matrix(0, nrow = size, ncol = size)
+
+  # Generate associated k's, j's, l's and m's (look-up table)
+  k <- c(unlist(lapply(1:dim, FUN = function(i) rep(i, dim - i + 1))), 1:dim) #plus  X'y
+  j <- unlist(lapply(1:dim, FUN = function(i) c(i:dim)))
+
+  # m is first x, j is second x (for columns)
+  m <- c(unlist(lapply(1:dim, FUN = function(i) rep(i, dim - i + 1))), 1:dim)
+  l <- unlist(lapply(1:dim, FUN = function(i) c(i:dim)))
+
+
+  # Build the covariance matrix
+  for(r in 1:size){
+    for(c in 1:size){
+      if(r >= threshold & c < threshold){ # then we are in Cov(X'y, X'X) world
+        Sigma[r, c] <-  as.numeric(covXyXX(k = k[r], j = m[c], m = l[c], S = S, X = X, Y = Y, N = N))
+      }else{
+        if(r < threshold & c >= threshold){ # then we are in Cov(X'X, X'y) world
+          Sigma[r, c] <-  as.numeric(covXyXX(k = m[c], j = k[r], m = j[r], S = S, X = X, Y = Y, N = N))
+        }else{
+          if(r < threshold & c < threshold){ # then we are in cov(X'X, X'X) world
+            Sigma[r, c] <- as.numeric(covXX(k = k[r], j =  j[r], m = m[c], l = l[c], S = S, X = X, x_prime_x, N = N))
+          }else{ # then we are in cov(X'y, X'y) world
+            Sigma[r, c] <- as.numeric(covXyXy(k = k[r], j = m[c], S = S, X = X, Y = Y, sigma_sq = sigma_sq,  x_prime_x, N = N))
+          }
+        }
+      }
+    }
+  }
+  # Now we have the covariance matrix and the mean vector; run simulations
+  sims <- do.call(rbind, lapply(1:nsims, FUN = function(i) simulateEst(X = X, mu = mu, Sigma = Sigma, S = S, N = N)))
+
+  return(list(sims = sims, Sigma = Sigma))
+}
+
+
+#' Bootstrap variance
+#'
+#' This function takes repeated samples from the data to estimate a bootstrap
+#' distribution of \eqn{\tilde{\beta}} and b.
+#' @param Y Dependent variable from OLS model
+#' @param X Design matrix that produced OLS coefficients
+#' @param S Matrix of error terms. Dimensions must be length(coef) x length(coef)
+#' @param nsims Number of times to draw from a MVN
+#' @param int_vars Vector of interaction variables
+#' @param sq_vars Vector of squared variables
+#' @param int1 Column index of first interaction variable
+#' @param int2 Column index of second interaction variable
+#' @param index_sq Column index of second interaction variable
+#' @return Bootstrap samples of b and \eqn{\tilde{\beta}}
+varianceBoot <- function(Y, X, S, nsims, N, int_vars, sq_vars, int1, int2, index_sq){
+
+  boot_dist <- do.call(rbind, lapply(1:nsims, FUN = function(i){
+
+    # Resample data
+    sample_i <- sample(1:N, N, replace = TRUE)
+    X_star <- X[sample_i, ]
+    Y_star <- Y[sample_i]
+
+    # Caculate estimates
+    b_star <- as.numeric(coef(lm(Y_star ~ X_star[, -1])))
+
+    if(length(int_vars) == 0 & length(sq_vars) == 0){
+      bias_correct <- betaTilde(coef = b_star, S = S, X = X_star, N = N)
+      bc_star <- bias_correct$bias_correct
+    }else{
+      if(length(int1) > 0){
+        bias_correct <- betaTildeInt(coef = b_star, S = S, X = X_star, N = N, int1 = int1, int2 = int2)
+        bc_star <- bias_correct$bias_correct
+      }else{
+        bias_correct <- betaTildeSq(coef = b_star, S = S, X = X_star, N = N, index_sq = index_sq)
+        bc_star <- bias_correct$bias_correct
+      }
+    }
+
+    return(c(b_star, bc_star))
+  }))
+
+  return(boot_dist)
+}
+
