cfc00a0273419b650c3f0b747a2d72f68a89d93e
georgieevans <georginaevans@g.harvard.edu>
2020-01-30 20:05:28 -0500

    fixing simulate data function

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -16,11 +16,10 @@ genData <- function(N)
   # Draw the true X's
   Z1_mean <- 7
   Z2_mean <- 9
-  Z3_mean <- 14
+  Z3_mean <- 10
   Z1 <- rpois(N, Z1_mean)
   Z2 <- Z1*corr + rpois(N, Z2_mean)
   Z3 <- Z2*corr + rpois(N, Z3_mean)
-  Z<- cbind(Z1, Z2)
 
   b0 <- 10
   b1 <- 12
@@ -35,15 +34,12 @@ genData <- function(N)
   s1 <- 2
   s2 <- 1
   s3 <- 1
-  V1 <- rnorm(N, 0, s1)
-  V2 <- rnorm(N, 0, s2)
-  V3 <- rnorm(N, 0, s3)
   X1 <- Z1 + rnorm(N, 0, s1)
   X2 <- Z2 + rnorm(N, 0, s2)
-  X3 <- Z3 <- rnorm(N, 0, s3)
+  X3 <- Z3 + rnorm(N, 0, s3)
 
   data_error <- data.frame(Y = Y, X1 = X1, X2 = X2, X3 = X3)
-  err_vec <- data.frame(Y = 0, X1 = s1^2, X2 = s2^2, X3 = s3^3)
+  err_vec <- data.frame(Y = 0, X1 = s1^2, X2 = s2^2, X3 = s3^2)
   data_error <- as.data.frame(rbind(err_vec, data_error))
 
   return(list(original_data = original_data, data_error = data_error))
