6a65c7deb18fdb2eac3e50165629393c8abeed74
georgieevans <georginaevans@g.harvard.edu>
2020-01-31 21:55:21 -0500

    updating data again

--- R/data-raw.R
+++ R/data-raw.R
@@ -1,7 +1,7 @@
 #set.seed(02138)
 #dat <- genData(100000)
-#private_data <- dat$original_data
-#dp_data <- dat$data_error
+#private_data <- dat$private_data
+#dp_data <- dat$dp_data
 #
 #usethis::use_data(private_data, overwrite = TRUE)
 #usethis::use_data(dp_data, overwrite = TRUE)
