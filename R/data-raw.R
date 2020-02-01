569e7a7db1307704052101d4210370b218666e36
georgieevans <georginaevans@g.harvard.edu>
2020-01-31 20:53:42 -0500

    editing data-raw

--- R/data-raw.R
+++ R/data-raw.R
@@ -1,7 +1,7 @@
-set.seed(02138)
-dat <- genData(100000)
-private_data <- dat$private_data
-dp_data <- dat$dp_data
+#set.seed(02138)
+#dat <- genData(100000)
+#private_data <- dat$private_data
+#dp_data <- dat$dp_data
 
 #usethis::use_data(private_data)
 #usethis::use_data(dp_data)
