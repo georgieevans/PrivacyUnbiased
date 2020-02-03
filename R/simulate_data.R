faed8254680a6899056825166ad9e4decdc50fc3
georgieevans <georginaevans@g.harvard.edu>
2020-02-02 19:15:06 -0500

    updating data simulation

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -11,7 +11,7 @@
 genData <- function(N)
   {
   Y_err <- 2
-  corr <- 2
+  corr <- 1
 
   # Draw the true X's
   Z1_mean <- 7
@@ -19,20 +19,20 @@ genData <- function(N)
   Z3_mean <- 3
   Z1 <- rpois(N, Z1_mean)
   Z2 <- Z1*corr + rpois(N, Z2_mean)
-  Z3 <- Z2*corr + rpois(N, Z3_mean)
+  Z3 <- rpois(N, Z3_mean)
 
   b0 <- 10
   b1 <- 12
   b2 <- -3
-  b3 <- 4
+  b3 <- 9
 
   Y_res <- rnorm(N, mean = 0, sd = Y_err)
   Y <- b0 + b1*Z1 + b2*Z2 + b3*Z3 + Y_res
 
   original_data <- data.frame(Y = Y, Z1 = Z1, Z2 = Z2, Z3 = Z3)
 
-  s1 <- 2
-  s2 <- 1
+  s1 <- 0.7
+  s2 <- 1.2
   s3 <- 1
   X1 <- Z1 + rnorm(N, 0, s1)
   X2 <- Z2 + rnorm(N, 0, s2)
